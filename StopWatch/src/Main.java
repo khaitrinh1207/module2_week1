1fb0883b634346f9962fa94be00acbba1dbabd76
khaitrinh1207 <khaitrinh1207@gmail.com>
2020-12-18 15:10:15 +0700

    add file

--- StopWatch/src/Main.java
+++ StopWatch/src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) {
+        StopWatch stopWatch=new StopWatch();
+        stopWatch.start();
+        int [] array=new int[200000];
+        for (int i = 0; i <200000 ; i++) {
+            array[i]=i+1;
+        }
+        Arrays.sort(array);
+        stopWatch.stop();
+        System.out.println(stopWatch.getElapsedTime());
+
+    }
 }
--- StopWatch/src/StopWatch.java
+++ StopWatch/src/StopWatch.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class StopWatch {
+import java.time.LocalTime;
+import java.util.Date;
+
+public class StopWatch {
+    private Date startTime;
+    private Date endTime;
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    StopWatch(){
+        this.startTime = new Date();
+    }
+
+    public void start(){
+        this.startTime = new Date();
+    }
+
+    public void stop(){
+        this.endTime = new Date();
+    }
+
+    public long getElapsedTime(){
+        return this.endTime.getTime()- this.startTime.getTime();
+    }
 }
