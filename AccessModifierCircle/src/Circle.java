1cdd58ddcc81487f89ccaf58f7130d8e39fd0cea
khaitrinh1207 <khaitrinh1207@gmail.com>
2020-12-18 16:39:19 +0700

    add file Circle2

---
+++ AccessModifierCircle/src/Circle.java
@@ -0,0 +1,41 @@
+public class Circle {
+    private double radius = 1;
+    private String color = "red";
+
+    public Circle(){}
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getArea(double radius){
+        return radius*radius*Math.PI;
+    }
+
+    public double getPerimeter(double radius){
+        return radius*2*Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ AccessModifierCircle/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10);
+        System.out.println(circle);
+    }
+}
